6bf97b7b1c7d35a196bec64f46c24e6f7158fb6c
Stephen Sun <stephensun.me@gmail.com>
2022-06-29 23:20:15 -0500

    add alt approaches for reverse linked list

--- Linked List/reverse_linked_list.js
+++ Linked List/reverse_linked_list.js
@@ -3,8 +3,13 @@
 
 // Tip: Keep track of previous, current, and next positions
 
+// Iterative Approach
 // Time Complexity: O(N)
 // Space Complexity: O(1)
+
+// Recursive Approach
+// Time Complexity: O(N)
+// Space Complexity: O(N)
 
 /**
  * Definition for singly-linked list.
@@ -17,6 +22,8 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+
+// Iterative
 var reverseList = function(head) {
   if (!head) return null
   
@@ -31,4 +38,28 @@ var reverseList = function(head) {
   }
   
   return prev
+};
+
+// ES6
+var reverseListAlt = function(head) {
+    let [prev, current] = [null, head]
+    while(current) {
+        [current.next, prev, current] = [prev, current, current.next]
+    }
+    return prev
+}
+
+// Recursive
+var reverseListAlt2 = function(head) {
+	// base case
+    if (head == null || head.next == null){
+        return head;
+    }
+	// go all the way to the end
+    let reversedListHead = reverseList(head.next)
+	// add reverse myself
+    head.next.next = head;
+    head.next = null;
+	// go up
+    return reversedListHead
 };
